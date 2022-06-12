62e93e556db50be5835f04aee6cb182958676fa6
Adebola <adebolaakomolafe@gmail.com>
2022-06-12 21:49:58 +0100

    Update reverse_string.rs

--- src/strings/reverse_string.rs
+++ src/strings/reverse_string.rs
@@ -1,12 +1,14 @@
-/// Prompt: Write a rust function that reverses a given string.
+/// #### Prompt
+/// Write a rust function that reverses a given string.
+/// #### Solution
+/// First simulate all characters of the string into an iterator.
+/// Reverse the iterator and collect the iterator values as a String.
 /// ## Example
 /// ```rust
 /// use rust_algorithms::reverse_string;
 /// assert_eq!(reverse_string("I am"), "ma I");
 /// ```
 pub fn reverse_string(input: &str) -> String {
-    // First simulate all characters of the string into an iterator.
-    // Reverse the iterator and collect the iterator values as a String.
     input.chars().rev().collect()
 }
 
